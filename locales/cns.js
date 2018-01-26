19f0ad5203ea040817f8621df57512f2420fa626
Jay Qiu <uwjjqiu@hotmail.com>
2018-01-26 01:07:00 -0500

    apply translation on input value

--- locales/cns.js
+++ locales/cns.js
@@ -1,9 +1,10 @@
 const cns_translation = {
   "title" : "初创智囊",
   "language" : "语言",
-  "continue" : "继续 &#8680;",
+  "continue" : "继续",
   "return" : "返回",
   "print" : "打印",
+  "send" : "发送",
   "ten_things_message" : "开始初创公司之前的10件事:",
 	"ten_things_1" : "理清经营的目的",
 	"ten_things_2" : "为此规划出10年的时间",
